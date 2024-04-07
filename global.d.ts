72bc8ce5a85d9b8e2d2c93e14432e7706adb6409
CPunisher <1343316114@qq.com>
2024-04-07 20:31:37 +0800

    Statically import .all-contributorsrc

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,8 @@
 // Use type safe message keys with `next-intl`
 type Messages = typeof import("./messages/en.json");
 declare interface IntlMessages extends Messages {}
+
+declare module "*.all-contributorsrc" {
+  const value: any;
+  export default value;
+}
