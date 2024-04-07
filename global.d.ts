1498e0580e646d1230b1b2501e48d2cb04818161
CPunisher <1343316114@qq.com>
2024-04-07 22:22:57 +0800

    Add types for .all-contributorsrc

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,22 @@ type Messages = typeof import("./messages/en.json");
 declare interface IntlMessages extends Messages {}
 
 declare module "*.all-contributorsrc" {
-  const value: any;
+  const value: {
+    projectName: string;
+    projectOwner: string;
+    repoType: string;
+    repoHost: string;
+    files: string[];
+    contributors: {
+      login: string;
+      name: string;
+      avatar_url: string;
+      profile: string;
+      contributions: string[];
+    }[];
+    commitType: string;
+    commitConvention: string;
+    contributorsPerLine: number;
+  };
   export default value;
 }
