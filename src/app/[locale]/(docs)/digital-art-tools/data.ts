650cfccc4b1daf0f2d44e73c00b9f495963e335f
倪豪 <1272777550@qq.com>
2024-04-11 04:55:50 +0800

    update digital art

---
+++ src/app/[locale]/(docs)/digital-art-tools/data.ts
@@ -0,0 +1,157 @@
+import { ResourceItem } from "@/lib/data_types";
+export const dataArt: ResourceItem[] = [
+  { id: "touchdesigner", name: "Touchdesigner", url: "https://derivative.ca/" },
+  { id: "processing", name: "Processing", url: "https://processing.org/" },
+  { id: "p5-js", name: "p5.js", url: "https://p5js.org/" },
+  { id: "open-framework", name: "Open Framework", url: "https://openframeworks.cc/" },
+  { id: "netlogo", name: "NetLogo", url: "https://ccl.northwestern.edu/netlogo/" },
+  { id: "glchaos-p", name: "glChAoS.P", url: "https://github.com/BrutPitt/glChAoS.P" },
+  { id: "jsplacement", name: "JSplacement", url: "https://windmillart.net/?p=jsplacement" },
+  { id: "unfolder", name: "Unfolder", url: "https://www.unfolder.app/" },
+  { id: "vvvv", name: "VVVV", url: "https://vvvv.org/" },
+  { id: "notch", name: "Notch", url: "https://www.notch.one/" },
+];
+
+export const data3dEngine: ResourceItem[] = [
+  { id: "houdini", name: "Houdini", url: "https://www.sidefx.com/" },
+  { id: "rhino", name: "Rhino", url: "https://www.rhino3d.com/" },
+  { id: "grasshopper", name: "Grasshopper", url: "https://www.grasshopper3d.com/" },
+  { id: "unity", name: "Unity", url: "https://unity.com/" },
+  { id: "unreal", name: "Unreal", url: "https://www.unrealengine.com/" },
+  { id: "blender", name: "Blender", url: "https://www.blender.org/" },
+  { id: "magicavoxel", name: "MagicaVoxel", url: "https://ephtracy.github.io/" },
+  { id: "spline", name: "Spline", url: "https://spline.design/" },
+  { id: "cinema-4d", name: "Cinema 4D", url: "https://www.maxon.net/en/cinema-4d" },
+  { id: "maya", name: "Maya", url: "https://www.autodesk.com/products/maya/overview" },
+  { id: "katana", name: "Katana", url: "https://www.foundry.com/products/katana" },
+  { id: "3ds-max", name: "3DS Max", url: "https://www.autodesk.com/products/3ds-max/overview" },
+  { id: "omniverse", name: "Omniverse", url: "https://developer.nvidia.com/nvidia-omniverse-platform" },
+  { id: "twinmotion", name: "Twinmotion", url: "https://www.unrealengine.com/en-US/twinmotion" },
+];
+
+export const data3dModeling: ResourceItem[] = [
+  { id: "shapr3d", name: "Shapr3D", url: "https://www.shapr3d.com/" },
+  { id: "zbrush", name: "ZBrush", url: "https://pixologic.com/zbrush/" },
+  { id: "marmoset", name: "Marmoset", url: "https://www.marmoset.co/" },
+  { id: "sketch-up", name: "SketchUp", url: "https://www.sketchup.com/" },
+  { id: "solidworks", name: "Solidworks", url: "https://www.solidworks.com/" },
+  { id: "fusion360", name: "Fusion360", url: "https://www.autodesk.com/products/fusion-360/overview" },
+  { id: "modo", name: "Modo", url: "https://www.foundry.com/products/modo" },
+  { id: "quixel", name: "Quixel", url: "https://quixel.com/" },
+];
+
+export const dataTexture: ResourceItem[] = [
+  { id: "substance-designer", name: "Substance Designer", url: "https://www.adobe.com/products/substance3d-designer.html" },
+  { id: "substance-painter", name: "Substance Painter", url: "https://www.adobe.com/products/substance3d-painter.html" },
+  { id: "substance-sampler", name: "Substance Sampler", url: "https://www.adobe.com/products/substance3d-sampler.html" },
+];
+
+export const dataRenderer: ResourceItem[] = [
+  { id: "redshift", name: "Redshift", url: "https://www.redshift3d.com/" },
+  { id: "eevee", name: "Eevee", url: "https://www.blender.org/features/eevee/" },
+  { id: "iray", name: "Iray", url: "https://www.nvidia.com/en-us/design-visualization/iray/" },
+  { id: "arnold", name: "Arnold", url: "https://www.arnoldrenderer.com/" },
+  { id: "v-ray", name: "V-Ray", url: "https://www.chaos.com/vray" },
+  { id: "octane", name: "Octane", url: "https://home.otoy.com/" },
+  { id: "keyshot", name: "Keyshot", url: "https://www.keyshot.com/" },
+  { id: "d5", name: "D5", url: "https://www.d5techs.com/" },
+  { id: "mantra", name: "Mantra", url: "https://www.sidefx.com/docs/houdini/render/overview.html" },
+  { id: "karma", name: "Karma", url: "https://www.sidefx.com/docs/houdini/render/karma.html" },
+  { id: "three-js", name: "Three.js", url: "https://threejs.org/" },
+];
+
+export const dataComputing: ResourceItem[] = [
+  { id: "cuda", name: "CUDA", url: "https://developer.nvidia.com/cuda-zone" },
+  { id: "metal", name: "Metal", url: "https://developer.apple.com/metal/" },
+  { id: "direct-x", name: "DirectX", url: "https://developer.microsoft.com/en-us/windows/directx/" },
+  { id: "vulkan", name: "Vulkan", url: "https://www.khronos.org/vulkan/" },
+  { id: "webgpu", name: "WebGPU", url: "https://gpuweb.github.io/" },
+  { id: "webgl", name: "webgl", url: "https://www.khronos.org/webgl/" },
+  { id: "opengl", name: "OpenGL", url: "https://www.opengl.org/" },
+  { id: "opencl", name: "OpenCL", url: "https://www.khronos.org/opencl/" },
+  { id: "taichi", name: "Taichi", url: "https://taichi.graphics/" },
+];
+
+export const dataGeometry: ResourceItem[] = [
+  { id: "mathematica", name: "Mathematica", url: "https://www.wolfram.com/mathematica/" },
+  { id: "geogebra", name: "Geogebra", url: "https://www.geogebra.org/" },
+];
+
+export const dataInteraction: ResourceItem[] = [
+  { id: "arduino", name: "Arduino", url: "https://www.arduino.cc/" },
+  { id: "wekinator", name: "Wekinator", url: "http://www.wekinator.org/" },
+  { id: "jockey", name: "Jockey", url: "https://apps.apple.com/us/app/jockey-osc/id1553621603" },
+  { id: "scratch", name: "Scratch", url: "https://scratch.mit.edu/" },
+];
+
+export const dataProjection: ResourceItem[] = [
+  { id: "arena", name: "Arena", url: "https://www.rockwellautomation.com/en-us/products/software/factorytalk/operationsuite/arena.html" },
+  { id: "madmapper", name: "MadMapper", url: "https://madmapper.com/" },
+];
+
+export const dataAudio: ResourceItem[] = [
+  { id: "max-msp", name: "Max/MSP", url: "https://cycling74.com/products/max" },
+  { id: "live", name: "Live", url: "https://www.ableton.com/en/live/" },
+  { id: "pure-data", name: "Pure Data", url: "https://puredata.info/" },
+  { id: "logic-pro", name: "Logic Pro", url: "https://www.apple.com/logic-pro/" },
+  { id: "garage-band", name: "Garage Band", url: "https://www.apple.com/mac/garageband/" },
+  { id: "izotope", name: "iZotope", url: "https://www.izotope.com/" },
+];
+
+export const dataDesign: ResourceItem[] = [
+  { id: "figma", name: "Figma", url: "https://www.figma.com/" },
+  { id: "sketch", name: "Sketch", url: "https://www.sketch.com/" },
+  { id: "Illustrator", name: "Illustrator", url: "https://www.adobe.com/products/illustrator.html" },
+  { id: "affinity-designer", name: "Affinity Designer", url: "https://affinity.serif.com/en-us/designer/" },
+  { id: "glyphs", name: "Glyphs", url: "https://glyphsapp.com/" },
+];
+
+export const dataPrototype: ResourceItem[] = [
+  { id: "protopie", name: "Protopie", url: "https://www.protopie.io/" },
+];
+
+export const dataPhotoEditing: ResourceItem[] = [
+  { id: "photoshop", name: "Photoshop", url: "https://www.adobe.com/products/photoshop.html" },
+  { id: "lightroom", name: "Lightroom", url: "https://www.adobe.com/products/photoshop-lightroom.html" },
+  { id: "pixelmator", name: "Pixelmator", url: "https://www.pixelmator.com/" },
+  { id: "affinity-photo", name: "Affinity Photo", url: "https://affinity.serif.com/en-us/photo/" },
+  { id: "gimp", name: "GIMP", url: "https://www.gimp.org/" },
+];
+
+export const dataLayoutDesign: ResourceItem[] = [
+  { id: "indesign", name: "InDesign", url: "https://www.adobe.com/products/indesign.html" },
+];
+
+export const dataVideoEditing: ResourceItem[] = [
+  { id: "premiere-pro", name: "Premiere Pro", url: "https://www.adobe.com/products/premiere.html" },
+  { id: "jian-ying", name: "剪映", url: "https://www.jianshu.com/p/7e6589303cf3" },
+  { id: "davinci-resolve", name: "Davinci Resolve", url: "https://www.blackmagicdesign.com/products/davinciresolve/" },
+  { id: "final-cut-pro", name: "Final Cut Pro", url: "https://www.apple.com/final-cut-pro/" },
+  { id: "imovie", name: "iMovie", url: "https://www.apple.com/imovie/" },
+];
+
+export const dataComposing: ResourceItem[] = [
+  { id: "nuke", name: "Nuke", url: "https://www.foundry.com/products/nuke-family/nuke" },
+  { id: "after-effect", name: "After Effect", url: "https://www.adobe.com/products/aftereffects.html" },
+];
+
+const data = {
+  dataArt,
+  data3dEngine,
+  data3dModeling,
+  dataTexture,
+  dataRenderer,
+  dataComputing,
+  dataGeometry,
+  dataInteraction,
+  dataProjection,
+  dataAudio,
+  dataDesign,
+  dataPrototype,
+  dataPhotoEditing,
+  dataLayoutDesign,
+  dataVideoEditing,
+  dataComposing,
+}
+
+export default data
--- src/app/[locale]/(docs)/digital-art-tools/page.tsx
+++ src/app/[locale]/(docs)/digital-art-tools/page.tsx
@@ -3,17 +3,67 @@ import { getTranslations } from "next-intl/server";
 import { SectionTitle } from "@/components/collection/SectionTitle";
 import { useTranslations } from "next-intl";
 
+import data from "./data";
+import { ProductView } from "@/components/collection/ProductView";
 
 export default function Page() {
   const t = useTranslations("docs.digital-art-tools")
+  const assetsPrefix = "/assets/collections/digital-art-tools"
 
   return (
     <CollectionLayout
       title={t("title")}
       intro={t("desc")}
+      authors={["ciaochaos", "vzhao-21"]}
     >
 
-      <SectionTitle title={t("title")} />
+      <SectionTitle title={t("gen_art")} />
+      <ProductView data={data.dataArt} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("3d_engine")} />
+      <ProductView data={data.data3dEngine} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("3d_modeling")} />
+      <ProductView data={data.data3dModeling} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("texture")} />
+      <ProductView data={data.dataTexture} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("renderer")} />
+      <ProductView data={data.dataRenderer} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("computing")} />
+      <ProductView data={data.dataComputing} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("geometry")} />
+      <ProductView data={data.dataGeometry} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("interaction")} />
+      <ProductView data={data.dataInteraction} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("projection")} />
+      <ProductView data={data.dataProjection} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("audio")} />
+      <ProductView data={data.dataAudio} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("graphics_design")} />
+      <ProductView data={data.dataDesign} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("prototype")} />
+      <ProductView data={data.dataPrototype} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("photo_editing")} />
+      <ProductView data={data.dataPhotoEditing} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("layout_design")} />
+      <ProductView data={data.dataLayoutDesign} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("video_editing")} />
+      <ProductView data={data.dataVideoEditing} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("composing")} />
+      <ProductView data={data.dataComposing} assetsPrefix={assetsPrefix} />
 
     </CollectionLayout>
   )
