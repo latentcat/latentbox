2156f60958b7714b1a95df1385d4f897ed8e313e
CPunisher <1343316114@qq.com>
2024-04-10 14:56:00 +0800

    Add data and UI components for Latent Cat Stack page

--- src/app/[locale]/(docs)/latent-cat-stack/page.tsx
+++ src/app/[locale]/(docs)/latent-cat-stack/page.tsx
@@ -2,21 +2,34 @@ import { CollectionLayout } from "@/components/SimpleLayout";
 import { getTranslations } from "next-intl/server";
 import { SectionTitle } from "@/components/collection/SectionTitle";
 import { useTranslations } from "next-intl";
-
+import { ProductView } from "@/components/collection/ProductView";
+import * as data from "./data";
 
 export default function Page() {
-  const t = useTranslations("docs.latent-cat-stack")
+  const t = useTranslations("docs.latent-cat-stack");
+  const assetsPrefix = "/assets/collections/latent-cat-stack";
 
   return (
-    <CollectionLayout
-      title={t("title")}
-      intro={t("desc")}
-    >
+    <CollectionLayout title={t("title")} intro={t("desc")}>
+      <SectionTitle title={t("framework")} />
+      <ProductView data={data.dataFramework} assetsPrefix={assetsPrefix} />
 
-      <SectionTitle title={t("title")} />
+      <SectionTitle title={t("deploy")} />
+      <ProductView data={data.dataDeploy} assetsPrefix={assetsPrefix} />
 
+      <SectionTitle title={t("ui")} />
+      <ProductView data={data.dataUI} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("library")} />
+      <ProductView data={data.dataLibrary} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("iconset")} />
+      <ProductView data={data.dataIconset} assetsPrefix={assetsPrefix} />
+
+      <SectionTitle title={t("analytics")} />
+      <ProductView data={data.dataAnalytics} assetsPrefix={assetsPrefix} />
     </CollectionLayout>
-  )
+  );
 }
 
 export async function generateMetadata({
@@ -24,7 +37,10 @@ export async function generateMetadata({
 }: Readonly<{
   params: { locale: string };
 }>) {
-  const t = await getTranslations({ locale, namespace: "docs.latent-cat-stack" });
+  const t = await getTranslations({
+    locale,
+    namespace: "docs.latent-cat-stack",
+  });
 
   return {
     title: t("title"),
