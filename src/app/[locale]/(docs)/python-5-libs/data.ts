b5ef7525cce8b87de8865325848ebd3070d32d2a
worksapce <289582304@qq.com>
2024-04-10 18:04:57 +0800

    feat: 增加python开发中个一个库

---
+++ src/app/[locale]/(docs)/python-5-libs/data.ts
@@ -0,0 +1,5 @@
+import { ResourceItem } from "@/lib/data_types";
+
+export const FaceRecognition: ResourceItem[] = [
+  {id: 'face-recognition', name: 'Face Recognition', url: 'https://github.com/ageitgey/face_recognition'}
+]
--- src/app/[locale]/(docs)/python-5-libs/page.tsx
+++ src/app/[locale]/(docs)/python-5-libs/page.tsx
@@ -2,10 +2,13 @@ import { CollectionLayout } from "@/components/SimpleLayout";
 import { getTranslations } from "next-intl/server";
 import { SectionTitle } from "@/components/collection/SectionTitle";
 import { useTranslations } from "next-intl";
+import { ProductView } from "@/components/collection/ProductView";
+import * as data from "./data"
 
 
 export default function Page() {
   const t = useTranslations("docs.python-5-libs")
+  const assetsPrefix =  "/assets/collections/python-5-libs"
 
   return (
     <CollectionLayout
@@ -13,7 +16,8 @@ export default function Page() {
       intro={t("desc")}
     >
 
-      <SectionTitle title={t("title")} />
+      <SectionTitle title={t("face_recognition")} />
+      <ProductView data={data.FaceRecognition} assetsPrefix={assetsPrefix} />
 
     </CollectionLayout>
   )
