482bdbf046210586b0f706aee147f391b447dff0
CPunisher <1343316114@qq.com>
2024-04-10 14:56:00 +0800

    Add data and UI components for Latent Cat Stack page

---
+++ src/app/[locale]/(docs)/latent-cat-stack/data.ts
@@ -0,0 +1,62 @@
+import { ResourceItem } from "@/lib/data_types";
+
+export const dataFramework: ResourceItem[] = [
+  { id: "nextjs", name: "Next.js", url: "https://nextjs.org/" },
+  { id: "react", name: "React", url: "https://react.dev/" },
+];
+
+export const dataDeploy: ResourceItem[] = [
+  { id: "vercel", name: "Vercel", url: "https://vercel.com/" },
+  { id: "railway", name: "Railway", url: "https://railway.app/" },
+];
+
+export const dataUI: ResourceItem[] = [
+  { id: "shadcn/ui", name: "shadcn/ui", url: "https://ui.shadcn.com/" },
+  { id: "tailwindcss", name: "Tailwind CSS", url: "https://tailwindcss.com/" },
+];
+
+export const dataLibrary: ResourceItem[] = [
+  { id: "jotai", name: "jotai", url: "https://jotai.org/" },
+  {
+    id: "framer-motion",
+    name: "framer-motion",
+    url: "https://www.framer.com/motion/",
+  },
+  { id: "next-auth", name: "next-auth", url: "https://next-auth.js.org/" },
+  {
+    id: "next-intl",
+    name: "next-intl",
+    url: "https://next-intl-docs.vercel.app/",
+  },
+  {
+    id: "react-hook-form",
+    name: "react-hook-form",
+    url: "https://react-hook-form.com/",
+  },
+  { id: "zod", name: "zod", url: "https://zod.dev/" },
+  { id: "zustand", name: "zustand", url: "https://zustand-demo.pmnd.rs/" },
+  {
+    id: "react-three-fiber",
+    name: "react-three-fiber",
+    url: "https://docs.pmnd.rs/react-three-fiber",
+  },
+  {
+    id: "@next/mdx",
+    name: "@next/mdx",
+    url: "https://nextjs.org/docs/pages/building-your-application/configuring/mdx",
+  },
+  {
+    id: "@tailwindcss/typography",
+    name: "@tailwindcss/typography",
+    url: "https://tailwindcss-typography.vercel.app/",
+  },
+];
+
+export const dataIconset: ResourceItem[] = [
+  { id: "heroicons", name: "Heroicons", url: "https://heroicons.com/" },
+  { id: "lucide-react", name: "lucide-react", url: "https://lucide.dev/" },
+];
+
+export const dataAnalytics: ResourceItem[] = [
+  { id: "mixpanel", name: "Mixpanel", url: "https://mixpanel.com/" },
+];
