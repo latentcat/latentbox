afa37819f9d1d1de622d9f7a6803ae0a1a004242
CPunisher <1343316114@qq.com>
2024-04-10 14:16:54 +0800

    Add contributing section in en and zh translations

--- src/app/[locale]/(docs)/docs/contributing/page.tsx
+++ src/app/[locale]/(docs)/docs/contributing/page.tsx
@@ -1,10 +1,19 @@
 import { ArticleLayout } from "@/components/SimpleLayout";
 import { getTranslations } from "next-intl/server";
-import { useTranslations } from "next-intl";
-import { Link } from "@/navigation";
+import { useLocale, useTranslations } from "next-intl";
+
+import zh from "./zh.mdx";
+import en from "./en.mdx";
+
+const localedPages = {
+  zh,
+  en,
+};
 
 export default function Page() {
+  const locale = useLocale();
   const t = useTranslations("docs.contributing");
+  const LocaledComponent = localedPages[locale as keyof typeof localedPages];
 
   return (
     <ArticleLayout
@@ -12,36 +21,7 @@ export default function Page() {
       intro=""
       authors={["cpunisher", "ciaochaos"]}
     >
-      <h2>{t("h2-1")}</h2>
-      <p>{t("p1-thanking")}</p>
-      <p>{t("p2-tip")}</p>
-      <p>
-        {t("p3-help")}
-        <Link href="https://twitter.com/nhciao" target="_blank">
-          @nhciao
-        </Link>
-      </p>
-      <h3>{t("h3-1")}</h3>
-      <p>{t("p4-fork")}</p>
-      <h3>{t("h3-2")}</h3>
-      <pre>
-        <code>git clone https://github.com/your-username/latentbox.git</code>
-      </pre>
-      <h3>{t("h3-3")}</h3>
-      <pre>
-        <code>cd latentbox</code>
-      </pre>
-      <h3>{t("h3-4")}</h3>
-      <pre>
-        <code>git checkout -b my-new-branch</code>
-      </pre>
-      <h3>{t("h3-5")}</h3>
-      <pre>
-        <code>yarn</code>
-      </pre>
-      <h3>{t("h3-6")}</h3>
-      <p>{t("p5-pr")}</p>
-      <p>{t("p6-accept")}</p>
+      <LocaledComponent />
     </ArticleLayout>
   );
 }
