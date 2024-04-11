dae977ca4867891d0bafbcad80db62f69dc8e3e2
CPunisher <1343316114@qq.com>
2024-04-11 14:45:14 +0800

    Update Next.js config with MDX and Shiki plugins

--- typography.ts
+++ typography.ts
@@ -298,15 +298,19 @@ export default function typographyStyles({ theme }: PluginUtils) {
         ":is(a, blockquote, thead th) strong": {
           color: "inherit",
         },
+        pre: {
+          padding: "1rem",
+          borderRadius: "0.5rem",
+        },
         code: {
-          color: "var(--tw-prose-code)",
-          borderRadius: theme("borderRadius.lg"),
-          paddingTop: theme("padding.1"),
-          paddingRight: theme("padding[1.5]"),
-          paddingBottom: theme("padding.1"),
-          paddingLeft: theme("padding[1.5]"),
-          boxShadow: "inset 0 0 0 1px var(--tw-prose-code-ring)",
-          backgroundColor: "var(--tw-prose-code-bg)",
+          // color: "var(--tw-prose-code)",
+          // borderRadius: theme("borderRadius.lg"),
+          // paddingTop: theme("padding.1"),
+          // paddingRight: theme("padding[1.5]"),
+          // paddingBottom: theme("padding.1"),
+          // paddingLeft: theme("padding[1.5]"),
+          // boxShadow: "inset 0 0 0 1px var(--tw-prose-code-ring)",
+          // backgroundColor: "var(--tw-prose-code-bg)",
           fontSize: theme("fontSize.xs"),
         },
         ":is(a, h1, h2, h3, blockquote, thead th) code": {
