dab850e119dd60729a83274bca7783ef2cd2e034
CPunisher <1343316114@qq.com>
2024-04-13 20:51:39 +0800

    Add radial cluster tree and update data in awesome-AI-products

--- src/app/[locale]/(docs)/awesome-ai-products/page.tsx
+++ src/app/[locale]/(docs)/awesome-ai-products/page.tsx
@@ -3,12 +3,23 @@ import { getTranslations } from "next-intl/server";
 import { SectionTitle } from "@/components/collection/SectionTitle";
 import { useTranslations } from "next-intl";
 import { ProductView } from "@/components/collection/ProductView";
-import data from "./data";
-
+import data, { json } from "./data";
+import RadialClusterTree from "@/components/RadialClusterTree";
 
 export default function Page() {
-  const t = useTranslations("docs.awesome-ai-products")
-  const assetsPrefix = "/assets/collections/awesome-ai-products"
+  const t = useTranslations("docs.awesome-ai-products");
+  const assetsPrefix = "/assets/collections/awesome-ai-products";
+
+  const radialClusterTreeData = {
+    name: t("title"),
+    children: json.map((d) => ({
+      name: t(d.category as never),
+      children: d.items.map((item) => ({
+        name: item.name,
+        value: 0,
+      })),
+    })),
+  };
 
   return (
     <CollectionLayout
@@ -16,6 +27,7 @@ export default function Page() {
       intro={t("desc")}
       authors={["HiMrHOW", "ciaochaos", "zheishei", "LonelyFellas"]}
     >
+      <RadialClusterTree data={radialClusterTreeData} />
 
       <SectionTitle title={t("llm-global")} />
       <ProductView data={data.dataLlmGlobal} assetsPrefix={assetsPrefix} />
@@ -46,9 +58,8 @@ export default function Page() {
 
       <SectionTitle title={t("design")} />
       <ProductView data={data.dataDesign} assetsPrefix={assetsPrefix} />
-
     </CollectionLayout>
-  )
+  );
 }
 
 export async function generateMetadata({
@@ -56,7 +67,10 @@ export async function generateMetadata({
 }: Readonly<{
   params: { locale: string };
 }>) {
-  const t = await getTranslations({ locale, namespace: "docs.awesome-ai-products" });
+  const t = await getTranslations({
+    locale,
+    namespace: "docs.awesome-ai-products",
+  });
 
   return {
     title: t("title"),
