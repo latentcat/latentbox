1cc062e9274aa18b77ad35e7a8669c11793bafd3
CPunisher <1343316114@qq.com>
2024-04-13 19:03:01 +0800

    Add d3 and RadialClusterTree component

--- src/app/[locale]/(docs)/sd-ecology/page.tsx
+++ src/app/[locale]/(docs)/sd-ecology/page.tsx
@@ -1,40 +1,61 @@
 import { CollectionLayout } from "@/components/SimpleLayout";
 import { getTranslations } from "next-intl/server";
 import { useTranslations } from "next-intl";
-import data from "./data.json";
 import { PaperView } from "@/components/collection/PaperView";
-
+import data from "./data.json";
+import RadialClusterTree from "@/components/RadialClusterTree";
 
 export default function Page() {
   const t = useTranslations("docs.sd-ecology");
   const assetsPrefix = "/assets/collections/sd-ecology";
 
+  const radialClusterTreeData = {
+    name: "Stable Diffusion",
+    children: data.map((d) => ({
+      name: d.category,
+      children: d.items.map((item) => ({
+        name: item.name,
+        value: 0,
+      })),
+    })),
+  };
+
   return (
     <CollectionLayout
       title={t("title")}
       intro={t("desc")}
-      authors={["ciaochaos", "chenbaiyujason", "huo-ju", "Dango233", "cpunisher", "Zhaohan-Wang", "gogodecay", "xiaohu2015", "sdbds", "IDKiro"]}
+      authors={[
+        "ciaochaos",
+        "chenbaiyujason",
+        "huo-ju",
+        "Dango233",
+        "cpunisher",
+        "Zhaohan-Wang",
+        "gogodecay",
+        "xiaohu2015",
+        "sdbds",
+        "IDKiro",
+      ]}
     >
-
+      <RadialClusterTree data={radialClusterTreeData} />
       <PaperView
         namespace="docs.sd-ecology"
         data={data}
         assetsPrefix={assetsPrefix}
       />
-
     </CollectionLayout>
   );
 }
 
 export async function generateMetadata({
-                                         params: { locale }
-                                       }: Readonly<{
+  params: { locale },
+}: Readonly<{
   params: { locale: string };
 }>) {
   const t = await getTranslations({ locale, namespace: "docs.sd-ecology" });
 
   return {
     title: t("title"),
-    description: t("desc")
+    description: t("desc"),
   };
 }
