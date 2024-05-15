b9b5ed033b53ef2c77b9bf403b25309c7fd1eae9
Vica Yang <22126501+VicaYang@users.noreply.github.com>
2024-05-15 21:21:43 +0800

    add content to the vis-tools page

---
+++ src/app/[locale]/(docs)/vis-tools/data.ts
@@ -0,0 +1,10 @@
+import { ResourceItem } from "@/lib/data_types";
+
+export const vistool: ResourceItem[] = [
+  { id: "D3", name: "D3", url: "https://d3js.org/", desc: "适合做具有复杂的动画、交互的可视化" },
+  { id: "Apache ECharts", name: "Apache ECharts", url: "https://echarts.apache.org/", desc: "适合做静态展示的图表" },
+  { id: "Vega", name: "Vega", url: "https://vega.github.io/vega/", desc: "用声明式语言创建可视化图表" },
+  { id: "Vega-Lite", name: "Vega-Lite", url: "https://vega.github.io/vega-lite/", desc: "更简洁易用的Vega" },
+  { id: "Observable", name: "Observable", url: "https://observablehq.com/", desc: "在线创建基于js的可视化界面" },
+  { id: "Tableau Public", name: "Tableau Public", url: "https://public.tableau.com/", desc: "免费的Tableau工具" },
+]
