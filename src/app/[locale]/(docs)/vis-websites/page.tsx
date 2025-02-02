1c91be9ccd0c867587036a7d5ac964fd1edfbf05
Leozzz7988 <166421489+Leozzz7988@users.noreply.github.com>
2025-02-02 16:25:37 -0500

    visualization websites update 1.0
    有些网页的链接没有找到, 部分名字可能有误, 部分链接暂时是空白的

--- src/app/[locale]/(docs)/vis-websites/page.tsx
+++ src/app/[locale]/(docs)/vis-websites/page.tsx
@@ -1,27 +1,28 @@
 import { CollectionLayout } from "@/components/SimpleLayout";
 import { getTranslations } from "next-intl/server";
-import { SectionTitle } from "@/components/collection/SectionTitle";
 import { useTranslations } from "next-intl";
+import { ProductView } from "@/components/collection/ProductView";
+import { visWebsite } from "./data";
 
 
 export default function Page() {
   const t = useTranslations("docs.vis-websites")
+  const assetsPrefix = "/assets/collections/vis-websites"
 
   return (
     <CollectionLayout
       title={t("title")}
       intro={t("desc")}
+      authors={["ciaochaos", "zheishei", "vzhao-21"]}
     >
-
-      <SectionTitle title={t("title")} />
-
+      <ProductView data={visWebsite} assetsPrefix={assetsPrefix} />
     </CollectionLayout>
   )
 }
 
 export async function generateMetadata({
-  params: { locale },
-}: Readonly<{
+                                         params: { locale },
+                                       }: Readonly<{
   params: { locale: string };
 }>) {
   const t = await getTranslations({ locale, namespace: "docs.vis-websites" });
